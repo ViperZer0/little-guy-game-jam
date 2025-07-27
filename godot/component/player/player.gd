3fd5d84c3a34b756a2fda4c71f99b298058f314a
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2025-07-27 11:35:25 -0700

    Added player sprites

--- godot/component/player/player.gd
+++ godot/component/player/player.gd
@@ -14,7 +14,8 @@ enum Direction {
 		upside_down = value
 		_set_upside_down(value)
 
-@onready var player_sprite: Sprite2D = %PlayerSprite
+@onready var torso_sprite: AnimatedSprite2D = %PlayerTorso
+@onready var head_sprite: AnimatedSprite2D = %PlayerBody
 @onready var player_detector: Area2D = %PlayerDetector
 
 ## This signal is fired when the two players touched!
@@ -116,8 +117,11 @@ func _set_upside_down(upside_down: bool) -> void:
 	# Flip gravity if needed.
 	gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * (-1 if upside_down else 1)
 	# Flip sprite
-	if player_sprite:
-		player_sprite.flip_v = upside_down
+	if torso_sprite:
+		torso_sprite.flip_v = upside_down
+
+	if head_sprite:
+		head_sprite.flip_v = upside_down
 
 	# Change up direction
 	up_direction = Vector2(0, 1 if upside_down else -1)
