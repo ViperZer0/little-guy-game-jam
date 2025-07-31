8349631c36a79c18962eecc20e45638ff0538c3e
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2025-07-30 17:32:37 -0700

    Fixed spinner behaving weirdly
    Spinner should now be using _physics_process instead of _process for
    calculating its rotation.

--- godot/component/spinner/spinner.gd
+++ godot/component/spinner/spinner.gd
@@ -3,5 +3,5 @@ extends AnimatableBody2D
 ## How fast the spinner should spin in degrees per second
 @export var spin_rate: float = 10.0
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	rotate(deg_to_rad(spin_rate) * delta)
